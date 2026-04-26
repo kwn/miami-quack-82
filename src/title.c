7e5feee7238986d6e6c41cadea784f3b623319cb
Karol Wnuk <k.wnuk@ascetic.pl>
2026-04-26 21:05:46 +0200

    Add menu

--- src/title.c
+++ src/title.c
@@ -50,25 +50,82 @@ typedef enum {
 } TitlePhase;
 
 typedef enum {
+    MENU_ACTION_NONE,
     MENU_ACTION_START,
     MENU_ACTION_OPTIONS,
-    MENU_ACTION_ENTER_CODE,
+    MENU_ACTION_MAIN,
+    MENU_ACTION_CODE,
     MENU_ACTION_EXIT
 } MenuAction;
 
+typedef enum {
+    MENU_ITEM_ACTION,
+    MENU_ITEM_OPTION
+} MenuItemKind;
+
+typedef enum {
+    MENU_PAGE_MAIN,
+    MENU_PAGE_OPTIONS
+} MenuPageId;
+
 typedef struct {
+    const char **labels;
+    UBYTE labelCount;
+    UBYTE *value;
+} MenuOption;
+
+typedef struct {
+    MenuItemKind kind;
     const char *text;
     MenuAction action;
+    MenuOption option;
 } MenuItem;
 
-static const MenuItem menuItems[] = {
-    { "START GAME", MENU_ACTION_START },
-    { "OPTIONS",    MENU_ACTION_OPTIONS },
-    { "ENTER CODE", MENU_ACTION_ENTER_CODE },
-    { "EXIT",       MENU_ACTION_EXIT },
+typedef struct {
+    const MenuItem *items;
+    UBYTE itemCount;
+} MenuPage;
+
+static UBYTE optDifficulty = 1;
+static UBYTE optControl = 0;
+static UBYTE optGore = 0;
+
+static const char *difficultyLabels[] = {
+    "DIFF: EASY",
+    "DIFF: NORMAL",
+    "DIFF: HARD",
+    "DIFF: INSANE",
 };
 
-#define MENU_ITEM_COUNT (sizeof(menuItems) / sizeof(menuItems[0]))
+static const char *controlLabels[] = {
+    "CTRL: KBD+MOUSE",
+    "CTRL: JOYSTICK",
+    "CTRL: JOY+AUTO",
+};
+
+static const char *goreLabels[] = {
+    "GORE: YES",
+    "GORE: NO",
+};
+
+static const MenuItem mainMenuItems[] = {
+    { MENU_ITEM_ACTION, "START GAME", MENU_ACTION_START,   { 0, 0, 0 } },
+    { MENU_ITEM_ACTION, "OPTIONS",    MENU_ACTION_OPTIONS, { 0, 0, 0 } },
+    { MENU_ITEM_ACTION, "ENTER CODE", MENU_ACTION_CODE,    { 0, 0, 0 } },
+    { MENU_ITEM_ACTION, "EXIT",       MENU_ACTION_EXIT,    { 0, 0, 0 } },
+};
+
+static const MenuItem optionsMenuItems[] = {
+    { MENU_ITEM_OPTION, 0,      MENU_ACTION_NONE, { difficultyLabels, 4, &optDifficulty } },
+    { MENU_ITEM_OPTION, 0,      MENU_ACTION_NONE, { controlLabels,    3, &optControl    } },
+    { MENU_ITEM_OPTION, 0,      MENU_ACTION_NONE, { goreLabels,       2, &optGore       } },
+    { MENU_ITEM_ACTION, "BACK", MENU_ACTION_MAIN, { 0, 0, 0 } },
+};
+
+static const MenuPage menuPages[] = {
+    { mainMenuItems,    sizeof(mainMenuItems) / sizeof(mainMenuItems[0]) },
+    { optionsMenuItems, sizeof(optionsMenuItems) / sizeof(optionsMenuItems[0]) },
+};
 
 static const UBYTE cursorAnimOffsets[] = {
     0, 0, 1, 2, 3, 4, 5, 5, 6, 6, 6, 5, 5, 4, 3, 2, 1, 0
@@ -96,12 +153,29 @@ static tTextBitMap *textBitmap;
 static UWORD pristinePalette[TITLE_COLOR_COUNT];
 
 static TitlePhase phase;
+static MenuPageId currentPage;
 static UBYTE selectedItem;
 static UBYTE cursorAnimFrame;
 static UBYTE cursorAnimTick;
 
 static void drawText(tBitMap *dest, UWORD x, UWORD y, const char *text, UBYTE color) {
     gameFontDrawStr(font, dest, textBitmap, x, y, text, color, COLOR_SHADOW);
+}
+
+static const MenuPage *getCurrentMenuPage(void) {
+    return &menuPages[currentPage];
+}
+
+static const MenuItem *getSelectedMenuItem(void) {
+    const MenuPage *page = getCurrentMenuPage();
+    return &page->items[selectedItem];
+}
+
+static const char *getMenuItemText(const MenuItem *item) {
+    if (item->kind == MENU_ITEM_OPTION) {
+        return item->option.labels[*item->option.value];
+    }
+    return item->text;
 }
 
 static void presentBackBuffer(void) {
@@ -181,13 +255,15 @@ static void applyCheckerOverlay(tBitMap *bitmap, UWORD x, UWORD y, UWORD width, 
 }
 
 static void drawMenuBase(tBitMap *dest) {
+    const MenuPage *page = getCurrentMenuPage();
+
     blitCopyAligned(titleBitmap, 0, 0, dest, 0, 0, SCREEN_W, SCREEN_H);
     blitWait();
     applyCheckerOverlay(dest, MENU_BOX_X, MENU_BOX_Y, MENU_BOX_W, MENU_BOX_H);
 
-    for (UBYTE i = 0; i < MENU_ITEM_COUNT; ++i) {
+    for (UBYTE i = 0; i < page->itemCount; ++i) {
         UWORD y = MENU_TEXT_Y + (i * MENU_TEXT_SPACING);
-        drawText(dest, MENU_TEXT_X, y, menuItems[i].text, COLOR_TEXT);
+        drawText(dest, MENU_TEXT_X, y, getMenuItemText(&page->items[i]), COLOR_TEXT);
     }
 }
 
@@ -195,8 +271,17 @@ static void drawFullMenu(tBitMap *dest) {
     drawMenuBase(dest);
 }
 
+static void refreshMenuPage(void) {
+    drawFullMenu(buffer->pBack);
+    updateCursorSprite();
+    presentBackBuffer();
+    vPortWaitForEnd(vport);
+    drawFullMenu(buffer->pBack);
+}
+
 static void showMenu(void) {
     phase = TITLE_PHASE_MENU;
+    currentPage = MENU_PAGE_MAIN;
     selectedItem = 0;
     cursorAnimFrame = 0;
     cursorAnimTick = 0;
@@ -212,32 +297,71 @@ static void showMenu(void) {
 }
 
 static void selectPrevious(void) {
+    const MenuPage *page = getCurrentMenuPage();
+
     if (selectedItem == 0) {
-        selectedItem = MENU_ITEM_COUNT - 1;
+        selectedItem = page->itemCount - 1;
     } else {
         --selectedItem;
     }
 }
 
 static void selectNext(void) {
+    const MenuPage *page = getCurrentMenuPage();
+
     ++selectedItem;
-    if (selectedItem >= MENU_ITEM_COUNT) {
+    if (selectedItem >= page->itemCount) {
         selectedItem = 0;
     }
 }
 
+static void changeOption(const MenuItem *item, BYTE delta) {
+    UBYTE value = *item->option.value;
+
+    if (delta < 0) {
+        value = (value == 0) ? item->option.labelCount - 1 : value - 1;
+    } else {
+        ++value;
+        if (value >= item->option.labelCount) {
+            value = 0;
+        }
+    }
+
+    *item->option.value = value;
+    refreshMenuPage();
+}
+
+static void showPage(MenuPageId page, UBYTE item) {
+    currentPage = page;
+    selectedItem = item;
+    refreshMenuPage();
+}
+
 static void activateSelected(void) {
-    switch (menuItems[selectedItem].action) {
+    const MenuItem *item = getSelectedMenuItem();
+
+    if (item->kind == MENU_ITEM_OPTION) {
+        changeOption(item, 1);
+        return;
+    }
+
+    switch (item->action) {
         case MENU_ACTION_START:
             fadePaletteLevel(MENU_DIM_LEVEL, PALETTE_BLACK_LEVEL);
             stateChange(stateMgr, &scrollerState);
+            break;
+        case MENU_ACTION_OPTIONS:
+            showPage(MENU_PAGE_OPTIONS, 0);
+            break;
+        case MENU_ACTION_MAIN:
+            showPage(MENU_PAGE_MAIN, 1);
             break;
         case MENU_ACTION_EXIT:
             fadePaletteLevel(MENU_DIM_LEVEL, PALETTE_BLACK_LEVEL);
             gameExit();
             break;
-        case MENU_ACTION_OPTIONS:
-        case MENU_ACTION_ENTER_CODE:
+        case MENU_ACTION_CODE:
+        case MENU_ACTION_NONE:
         default:
             break;
     }
@@ -277,6 +401,7 @@ static void titleCreate(void) {
     systemSetDmaBit(DMAB_SPRITE, 1);
 
     phase = TITLE_PHASE_SPLASH;
+    currentPage = MENU_PAGE_MAIN;
     selectedItem = 0;
     drawSplash();
     presentBackBuffer();
@@ -289,6 +414,7 @@ static void titleCreate(void) {
 
 static void titleLoop(void) {
     UBYTE selectionChanged = 0;
+    const MenuItem *selectedMenuItem;
 
     if (phase == TITLE_PHASE_SPLASH) {
         if (keyUse(KEY_RETURN) || keyUse(KEY_SPACE) || joyUse(JOY1_FIRE)) {
@@ -304,12 +430,20 @@ static void titleLoop(void) {
         return;
     }
 
+    selectedMenuItem = getSelectedMenuItem();
+
     if (keyUse(KEY_UP) || joyUse(JOY1_UP)) {
         selectPrevious();
         selectionChanged = 1;
     } else if (keyUse(KEY_DOWN) || joyUse(JOY1_DOWN)) {
         selectNext();
         selectionChanged = 1;
+    } else if ((keyUse(KEY_LEFT) || joyUse(JOY1_LEFT)) && selectedMenuItem->kind == MENU_ITEM_OPTION) {
+        changeOption(selectedMenuItem, -1);
+        return;
+    } else if ((keyUse(KEY_RIGHT) || joyUse(JOY1_RIGHT)) && selectedMenuItem->kind == MENU_ITEM_OPTION) {
+        changeOption(selectedMenuItem, 1);
+        return;
     }
 
     if (keyUse(KEY_RETURN) || keyUse(KEY_SPACE) || joyUse(JOY1_FIRE)) {
@@ -318,7 +452,11 @@ static void titleLoop(void) {
     }
 
     if (keyUse(KEY_ESCAPE)) {
-        gameExit();
+        if (currentPage == MENU_PAGE_OPTIONS) {
+            showPage(MENU_PAGE_MAIN, 1);
+        } else {
+            gameExit();
+        }
         return;
     }
 
